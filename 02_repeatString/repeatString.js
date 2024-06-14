88405b2913ffc53980a81f10a8cd5ba43a193886
danielrov <80066620+danielrov@users.noreply.github.com>
2024-06-14 17:22:34 -0300

    Solve for exercise 02_repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+ let newString = ""
+ if (num < 0) return "ERROR";
+ while (num > 0) {
+    newString = newString + string;
+    num--;
+ }
+ return newString
 };
 
 // Do not edit below this line
