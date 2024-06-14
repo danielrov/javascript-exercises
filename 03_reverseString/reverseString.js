722a04c1e91994b07cebcae3045e667c55f4172c
danielrov <80066620+danielrov@users.noreply.github.com>
2024-06-14 17:35:05 -0300

    Solve for exercise 03_reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+ letters = word.length
+ reverseWord = ""
+ while (letters > 0) {
+    reverseWord = reverseWord + word.charAt(letters-1);
+    letters--
+ }
+ return reverseWord
 };
 
 // Do not edit below this line
