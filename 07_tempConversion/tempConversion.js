d722bd60c64a5e1d37d6fac48a143e5c403a58b9
danielrov <80066620+danielrov@users.noreply.github.com>
2024-06-16 11:59:24 -0300

    Solve for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let toCelsius = (+farenheit - 32) / 1.8
+  return Math.round(toCelsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let toFahrenheit = celsius * 1.8 + 32
+  return Math.round(toFahrenheit * 10) / 10
 };
 
 // Do not edit below this line
