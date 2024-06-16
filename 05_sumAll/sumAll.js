1bbd335516616b5f00e0176154ad8cb1cd5ea257
danielrov <80066620+danielrov@users.noreply.github.com>
2024-06-16 11:35:35 -0300

    Solver for 05_sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+if (a < 0 || !Number.isInteger(a) || b < 0 || !Number.isInteger(b)) {
+     return "ERROR";
+} else {
+    let sum = 0
+    numberA = Math.min(a,b)
+    numberB = Math.max(a,b)
+    while (numberA <= numberB) {
+        sum = sum + numberA;
+        numberA++;
+        }
+    return sum
+}
 };
 
 // Do not edit below this line
