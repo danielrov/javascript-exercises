9df0d2eaa820b5ca664b5c395d5bbc841b49baf0
danielrov <80066620+danielrov@users.noreply.github.com>
2024-06-23 15:25:15 -0300

    Solver for 10_fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+
+    const array = [1 , 1];
+   
+    for (let index = 1; index <= +number; index++) {
+        array.push(array.at(-1) + array.at(-2))
+    }
+
+    if (number == 0) {
+        return 0
+    } if (number < 1) {
+        return "OOPS"
+    } else {
+        return array[number - 1]
+    }
 
 };
 
